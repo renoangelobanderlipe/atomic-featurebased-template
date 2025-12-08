59199e8c1904dcf1bdbc639cc2fd829a9d12031d
Reno Angelo Banderlipe <personal.renoangelo+dev@gmail.com>
2025-12-08 18:11:26 +0800

    chore: fix ci build error

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,14 +1,5 @@
 import Axios from "axios";
-import { getToken } from "@/features/auth/utils/authStorage";
 
-export const axios = Axios.create({
+export const apiClient = Axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "https://api.example.com",
-});
-
-axios.interceptors.request.use((config) => {
-  const token = getToken();
-  if (token && config.headers) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
 });
